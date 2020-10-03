a74906becfa3f9fbc78097dc63e339583b3e922a
USS_Punchline <rezanova990@gmail.com>
2020-10-04 00:57:49 +0330

    stage 4

---
+++ Assets/ship.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
